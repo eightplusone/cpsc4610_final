7574986048dbac3abd92f8deeede50e8bfbb1414
Tyler Hartje <hartjet@users.noreply.github.com>
2016-03-09 16:45:42 -0800

    Added files via upload
    added loadData functionality

---
+++ loadData.lua
@@ -0,0 +1,27 @@
+function loadData(fileLocation)
+	local file = io.open(fileLocation)
+	local ret = {}	
+
+	if file then
+		local lineCounter = 1
+		for line in file:lines() do
+			table.insert(ret, split(line))
+		end
+	end
+	return ret
+end
+
+function split(line)
+	local arr = {}
+
+	if line then
+		for i = 1, #line do
+			local str = line:sub(i,i)
+			if str ~= "\n" then
+				table.insert(arr, str)
+			end
+		end
+	return arr
+	end
+end
+
