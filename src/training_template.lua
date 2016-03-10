b8c93d501990d0c56c4d418b54ba3cb6962cb341
Hai Tran <hai.t@outlook.com>
2016-03-10 07:14:29 -0800

    Good to go

--- src/training_template.lua
+++ src/training_template.lua
@@ -19,7 +19,7 @@ require('loadData.lua')
 --
 local DATASET_SIZE = 90
 local INPUT_SIZE = 67
-local HIDDEN_LAYER_SIZE = 10    -- play with this number
+local HIDDEN_LAYER_SIZE = 6    -- play with this number
 local OUTPUT_SIZE = 6
 local LEARNING_RATE = 17e-3
 local LEARNING_RATE_DECAY = 0
@@ -72,11 +72,11 @@ local NUM_HIDDEN_LAYERS = 6
 -- Normal cases: please make sure you have the right number of hidden layers.
 -- The template below represents a neural net with six hidden layer.
 model:add(module_01)
-model:add(module_02)
-model:add(module_03)
-model:add(module_04)
-model:add(module_05)
-model:add(module_06)
+--model:add(module_02)
+--model:add(module_03)
+--model:add(module_04)
+--model:add(module_05)
+--model:add(module_06)
 model:add(module_07)
 model:add(module_out)
 
@@ -98,13 +98,28 @@ criterion.sizeAverage = false
 -- each subset contains 15 entries.
 -- In each subset, we pick 9 entries for the training set, 3 for
 -- validation, and 3 for testing.
+--
+-- UPDATE: We will not implement k-fold. We will just simple use 1-fold:
+-- 60% for training set, 20% for validation set, and 20% for test set.
 local data = loadData(DATAFILE)
-data = torch.Tensor(data)
-local input = data[{ {INPUT_SIZE+1} }]
-local labels = data[{ {1,INPUT_SIZE} }]
 --
 -- Generate a random permutation of a sequence between 1 and DATASET_SIZE
 local indices = torch.randperm(DATASET_SIZE)
+--
+-- Split data
+local trainSet, validationSet, testSet = {}, {}, {}
+for i = 1, DATASET_SIZE*0.6 + 1 do
+  table.insert(trainSet, data[indices[i]])
+end
+for i = DATASET_SIZE*0.6 + 1, DATASET_SIZE*0.8+1 do
+  table.insert(validationSet, data[indices[i]])
+end
+for i = DATASET_SIZE*0.8 + 1, DATASET_SIZE do
+  table.insert(testSet, data[indices[i]])
+end
+trainSet = torch.Tensor(trainSet)
+validationSet = torch.Tensor(validationSet)
+testSet  = torch.Tensor(testSet)
 
 ----------------------------------------------------------------------
 -- Training function
@@ -124,13 +139,13 @@ w, dl_dw = model:getParameters()
 -- The SGD (Stochastic Gradient Descent) optimization function in the
 -- optim library has the signature of:
 --     optim.sgd(func,x,state)
--- Normally, people change the name of "func" to "feval" to avoid duplication.
+-- Normally, people change the name of "func" to "opfunc" to avoid duplication.
 -- The below feval function computes the value of the loss function at a
 -- given point w, and the gradient of that function with respect to w.
 -- w is the vector of trainable weights, which are all the weights of
 -- the linear matrix of our net, plus one bias.
 -- w_new is the updated weights.
-feval = function(w_new)
+opfunc = function(w_new)
   -- Copy the weights if they were updated in the last iteration
   -- They are vectors (or Tensors in the world of Torch), so we need to use
   -- copy() funciton
@@ -140,9 +155,9 @@ feval = function(w_new)
 
   -- select a new training sample
   _nidx_ = (_nidx_ or 0) + 1
-  if _nidx_ > (#data)[1] then _nidx_ = 1 end
+  if _nidx_ > (#trainSet)[1] then _nidx_ = 1 end
 
-  local sample = data[_nidx_]
+  local sample = trainSet[_nidx_]
   local target = sample[{ {INPUT_SIZE+1} }]    -- this funny looking syntax allows
   local inputs = sample[{ {1,INPUT_SIZE} }]    -- slicing of arrays.
 
@@ -164,6 +179,55 @@ feval = function(w_new)
 end
 
 ----------------------------------------------------------------------
+-- Validate function
+--
+fval = function()
+  -- Set model to non-training mode
+  model:evaluate()
+
+  -- Store the cumulatedLoss of the validation set
+  local cumulatedLoss = 0
+  -- Loop through validation set
+  for i = 1, DATASET_SIZE*0.2 + 1 do
+    -- Load validation data
+    local sample = trainSet[i]
+    local target = sample[{ {INPUT_SIZE+1} }]    -- this funny looking syntax allows
+    local inputs = sample[{ {1,INPUT_SIZE} }]    -- slicing of arrays.
+    -- Calculate the loss
+    local prediction = model:forward(inputs)
+    local loss_w = criterion:forward(prediction, target)
+    -- Add to the cumulatedLoss
+    cumulatedLoss = cumulatedLoss + loss_w
+  end
+
+  -- Set model back to training mode
+  model:training()
+
+  return cumulatedLoss/(DATASET_SIZE*0.2)
+end
+
+----------------------------------------------------------------------
+-- Test function
+--
+ftest = function()
+  local correct = 0
+  for i = 1, DATASET_SIZE*0.2 do
+    -- Get inputs & target label
+    local sample = testSet[i]
+    local target = sample[{ {INPUT_SIZE+1} }]    -- this funny looking syntax allows
+    local inputs = sample[{ {1,INPUT_SIZE} }]    -- slicing of arrays.
+    -- Feed into network
+    local prediction = model:forward(inputs)
+    -- Check if prediction == target
+    local predictedLabel = math.floor(prediction[1] + 0.5)
+    if predictedLabel == target[1] then
+      correct = correct + 1
+    end
+  end
+  return (correct*100)/(DATASET_SIZE*0.2)
+end
+
+----------------------------------------------------------------------
 -- Explanation from https://raw.githubusercontent.com/andresy/torch-demos/master/linear-regression/example-linear-regression.lua
 -- Given the function above, we can now easily train the model using SGD.
 -- For that, we need to define four key parameters:
@@ -181,48 +245,106 @@ state = {
 }
 
 ----------------------------------------------------------------------
--- Training
+-- Run
 --
-timer = torch.Timer()
+for run = 1, RUNS_PER_MODEL do
+  -- Start a timer
+  local timer = torch.Timer()
+  -- Store the error of all epoches
+  local trainingLoss = {}
+  -- Store validation accuracy of all epoches
+  local validationLoss = {}
+  -- For early stopping
+  local bestAccuracy, bestEpoch = 20, 0
+  -- Save the best model into a file
+  local modelFile = '../model/model_' .. NUM_HIDDEN_LAYERS .. '_' .. HIDDEN_LAYER_SIZE
 
-for i = 1, MAX_EPOCH do
-  -- This variable is used to estimate the average loss
-  currentLoss = 0
+  for epoch = 1, MAX_EPOCH do
 
-  -- An epoch is a full loop over our training data
-  for i = 1, DATASET_SIZE*0.6 do
+    -- This variable is used to estimate the average loss
+    trainingLoss[epoch] = 0
 
-    -- The optim library contains several optimization algorithms.
-    -- All of these algorithms assume the same parameters:
-    --   + a closure that computes the loss, and its gradient wrt to w,
-    --     given a point w
-    --   + a point w
-    --   + state of the net, which are algorithm-specific
-    -- Functions in optim all return two things:
-    --   + the new w, found by the optimization method (here SGD)
-    --   + the value of the loss functions at all points that were used by
-    --     the algorithm. SGD only estimates the function once, so
-    --     that list just contains one value.
-    -- (w = weights)
-    w_new, fs = optim.sgd(feval, w, state)
+    -- An epoch is a full loop over our training data
+    for j = 1, DATASET_SIZE*0.6 + 1 do
 
-    currentLoss = currentLoss + fs[1]
+      -- The optim library contains several optimization algorithms.
+      -- All of these algorithms assume the same parameters:
+      --   + a closure that computes the loss, and its gradient wrt to w,
+      --     given a point w
+      --   + a point w
+      --   + state of the net, which are algorithm-specific
+      -- Functions in optim all return two things:
+      --   + the new w, found by the optimization method (here SGD)
+      --   + the value of the loss functions at all points that were used by
+      --     the algorithm. SGD only estimates the function once, so
+      --     that list just contains one value.
+      -- (w = weights)
+      w_new, fs = optim.sgd(opfunc, w, state)
+      -- Accumulate the loss
+      trainingLoss[epoch] = trainingLoss[epoch] + fs[1]
+    end
+
+    -- Report average error on epoch
+    trainingLoss[epoch] = trainingLoss[epoch] / DATASET_SIZE
+    print('Training loss = ' .. trainingLoss[epoch])
+
+    -- Validation
+    validationLoss[epoch] = fval()
+    print('Validation loss = ' .. validationLoss[epoch])
+
+    -- Early Stopping when no new maxima has been found in 10 epoches
+    local wait = 0
+    if epoch % 50 == 0 then
+      if trainingLoss[epoch] > trainingLoss[epoch-1] and
+          validationLoss[epoch] < bestAccuracy then
+        bestAccuracy, bestEpoch = validationLoss[epoch], epoch
+        torch.save(modelFile, model)
+        wait = 0
+      else
+        break
+      end
+    end
+
   end
 
-  -- report average error on epoch
-  currentLoss = currentLoss / DATASET_SIZE
-  print('current loss = ' .. currentLoss)
+  -- Get training time
+  local trainingTime = timer:time().real
+  print('Training time: ' .. trainingTime .. ' seconds')
+
+  -- Test
+  testAccuracy = ftest()
+  print('Test accuracy: ' .. testAccuracy)
+
+  -- Plot
+  local graphFile = '../result/graph_' .. NUM_HIDDEN_LAYERS .. '_' .. HIDDEN_LAYER_SIZE .. '_' .. run .. '.png'
+  gnuplot.pngfigure(graphFile)
+  gnuplot.title('Training & Validation Loss')
+  gnuplot.ylabel('Loss')
+  gnuplot.xlabel('Epoch (x5)')
+  -- Select data to plot (to make the graph clearer)
+  local tLoss, vLoss = {}, {}
+  for i = 1, #trainingLoss do
+    if i%5==0 then
+      tLoss[math.floor(i/5)] = trainingLoss[i]
+      vLoss[math.floor(i/5)] = validationLoss[i]
+    end
+  end
+  -- Add two lists to the drawing
+  gnuplot.plot(
+      {'Train Loss', torch.Tensor(tLoss)},
+      {'Validation Loss', torch.Tensor(vLoss)}
+  )
+  gnuplot.plotflush()
+
+  -- Log result
+  --
+  local logFile = '../result/result_' .. NUM_HIDDEN_LAYERS .. '_' .. HIDDEN_LAYER_SIZE .. '.txt'
+  local file, fileErr = io.open(logFile, 'a+')
+  if fileErr then print('File Open Error')
+  else
+    file:write('\n----- Run #' .. run .. ' -----\n')
+    file:write('Duration: ' .. trainingTime .. '\n')
+    file:write('Test Accuracy: ' .. testAccuracy .. '\n')
+    file:close()
+  end
 end
-
-print('Time elapsed: ' .. timer:time().real .. ' seconds')
-
-----------------------------------------------------------------------
--- Testing
-prediction = model:forward(data[19][{ {1,INPUT_SIZE} }])
-x = math.floor(prediction[1] + 0.5)
-print(x)
-
-----------------------------------------------------------------------
--- Log result
---
-local filename = '../result' .. NUM_HIDDEN_LAYERS .. HIDDEN_LAYER_SIZE .. ''
